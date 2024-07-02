b369e7043443065afd08b3a552715692c5db64fb
saya5119 <22144049@ed.tmu.ac.jp>
2024-07-02 17:25:39 +0900

    kadaitotyu

--- docs/src/routes/+page.svelte
+++ docs/src/routes/+page.svelte
@@ -1,2 +1,38 @@
-<h1>オノマトペ</h1>
-<p>オノマトペは、音を文字で表現したものです。</p>
+
+<script>
+
+</script>
+
+<svelte:head>
+  <title>オノマトペツール</title>
+</svelte:head>
+
+<div class="nyuryoku">
+    <p>生成したい文字を入力してください</p>
+    <input type="text" id="txet" maxlength="5" pattern="^[ぁ-んァ-ヶー]+$" placeholder="ひらがな、カタカナのみ五文字以内です"/>
+</div>
+
+<p id="msg"></p>
+
+
+<style>
+	p{
+		text-align: center;
+	}
+    input{
+        width: 30%;
+        margin: 0 auto;
+        padding: 0.5em;
+        font-size: medium;
+    }
+    input:invalid {
+	background-color: lightpink;
+    }
+    .nyuryoku{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 60vh;
+    }
+</style>
