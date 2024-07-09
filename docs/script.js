0fe0042dd0869d2867a9cd814e6194bda68c89d3
saya5119 <22144049@ed.tmu.ac.jp>
2024-07-09 17:51:07 +0900

    kadaitotyu3

--- docs/script.js
+++ docs/script.js
@@ -3,11 +3,12 @@ function buttonClick(event) {
     let msg = document.getElementById('msg');
     let regex = /^[ぁ-んァ-ヶー]+$/;
 
-    if (regex.test(myText.value)) {
+    if (regex.test(myText.value) && myText.value.length <= 5) {
         msg.textContent = myText.value;
         window.location.href = 'fontselect/index.html';
     } else {
         msg.textContent = "ひらがな、カタカナのみ五文字以内です";
+        myText.classList.add('invalid');
         event.preventDefault();  // Prevent the default action (e.g., form submission)
     }
 }
@@ -15,4 +16,14 @@ function buttonClick(event) {
 document.addEventListener('DOMContentLoaded', function() {
     let checkButton = document.getElementById('checkButton');
     checkButton.addEventListener('click', buttonClick);
+
+    document.getElementById('txet').addEventListener('input', function() {
+        let input = this;
+        let regex = /^[ぁ-んァ-ヶー]+$/;
+        if (input.value.length > 5 || !regex.test(input.value)) {
+            input.classList.add('invalid');
+        } else {
+            input.classList.remove('invalid');
+        }
+    });
 });
