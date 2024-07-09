37c6bb7ee13037c42b97b6c06aa46430a2bac205
saya5119 <22144049@ed.tmu.ac.jp>
2024-07-09 14:48:32 +0900

    kadaitotyu2

--- docs/script.js
+++ docs/script.js
@@ -5,11 +5,14 @@ function buttonClick(event) {
 
     if (regex.test(myText.value)) {
         msg.textContent = myText.value;
+        window.location.href = 'fontselect/index.html';
     } else {
         msg.textContent = "ひらがな、カタカナのみ五文字以内です";
         event.preventDefault();  // Prevent the default action (e.g., form submission)
     }
 }
 
-let checkButton = document.getElementById('checkButton');
-checkButton.addEventListener('click', buttonClick);
+document.addEventListener('DOMContentLoaded', function() {
+    let checkButton = document.getElementById('checkButton');
+    checkButton.addEventListener('click', buttonClick);
+});
